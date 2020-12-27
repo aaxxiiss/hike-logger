4b51c94f2a865e1ada212744e862e7ad14033b81
mail@jukkaisokoski.fi <jukka@Jukkas-MacBook-Pro.local>
2020-12-27 20:10:52 +0200

    added list of journals to index.html

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,67 @@
 console.log('client.js loaded');
 
+const JOURNAL_API_URL = 'http://localhost:5000/API/journals/';
+
 function isHomePage() {
     return document.location.pathname === '/';
 }
 
+function getJournals() {
+    fetch(JOURNAL_API_URL)
+        .then(response => response.json())
+        .then(journals => {
+            console.log(journals);
+            printJournals(journals);
+        })
+}
+
+function printJournals(journals) {
+    const journeyListDiv = document.getElementById('journal-list');
+
+    for (let journal of journals) {
+        let journalDiv = document.createElement('div');
+        journalDiv.classList.add('journal-item');
+
+        let journalUl = document.createElement('ul');
+        let nameLi = document.createElement('li');
+        let nameHeaderSpan = document.createElement('span');
+        nameHeaderSpan.classList.add('list-header');
+        nameHeaderSpan.textContent = 'Name';
+        nameLi.appendChild(nameHeaderSpan);
+        let nameDataSpan = document.createElement('span');
+        nameDataSpan.classList.add('list-data');
+        nameDataSpan.textContent = journal.name;
+        nameLi.appendChild(nameDataSpan);
+        journalUl.appendChild(nameLi);
+
+        let descriptionLi = document.createElement('li');
+        let descriptionHeaderSpan = document.createElement('span');
+        descriptionHeaderSpan.classList.add('list-header');
+        descriptionHeaderSpan.textContent = 'Description';
+        descriptionLi.appendChild(descriptionHeaderSpan);
+        let descriptionDataSpan = document.createElement('span');
+        descriptionDataSpan.classList.add('list-data');
+        descriptionDataSpan.textContent = journal.description;
+        descriptionLi.appendChild(descriptionDataSpan);
+        journalUl.appendChild(descriptionLi);
+
+        let dateLi = document.createElement('li');
+        let dateHeaderSpan = document.createElement('span');
+        dateHeaderSpan.classList.add('list-header');
+        dateHeaderSpan.textContent = 'Date';
+        dateLi.appendChild(dateHeaderSpan);
+        let dateDataSpan = document.createElement('span');
+        dateDataSpan.classList.add('list-data');
+        dateDataSpan.textContent = journal.created;
+        dateLi.appendChild(dateDataSpan);
+        journalUl.appendChild(dateLi);
+
+        journalDiv.appendChild(journalUl);
+        journeyListDiv.appendChild(journalDiv);
+    }
+}
+
 if (isHomePage()) {
     console.log('On home page');
+    getJournals();
 }
