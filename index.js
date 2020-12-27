cac91b75eac6ac1de7ba71619df3d49c58d5d08d
mail@jukkaisokoski.fi <jukka@Jukkas-MacBook-Pro.local>
2020-12-27 14:57:29 +0200

    npm installed morgan and nodemon

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('Listening at port 3000'))
+
+app.listen(5000, () => console.log('Listening at port 5000'));
+
 app.use(express.static('public'));
+
+app.get('/api/', (req, res) => {
+    res.json({
+        message: 'request passed to the server',
+    });
+});
