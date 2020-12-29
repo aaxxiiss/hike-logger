3b0ca500d31c5b0ed4c1ffb3b3a7aa9ed8b3bf6c
mail@jukkaisokoski.fi <jukka@Jukkas-MacBook-Pro.local>
2020-12-29 16:20:01 +0200

    get current location on logging page

--- public/js/log.js
+++ public/js/log.js
@@ -3,5 +3,35 @@ console.log('log.js loaded');
 
 const journalId = getJournalId();
 
+function setCoordinates(pos) {
+    const location = pos.coords;
+    document.getElementById('lat').value = location.latitude;
+    document.getElementById('lon').value = location.longitude;
+    document.getElementById('log-submit').disabled = false;
+}
+
+function error() {
+    console.error('Could not receive coordinates');
+    return -1;
+}
+
+function getLocation() {
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+
+    if ("geolocation" in navigator) {
+        console.log('Geolocation available');
+        return navigator.geolocation.getCurrentPosition(setCoordinates, error, options);
+    } else {
+        console.log('Geolocation not available');
+        return -1;
+    }
+
+}
 
 
+getLocation();
