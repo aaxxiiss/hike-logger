767c3ba8d0908f1a8a0b71e4f990d8ac369a48c9
mail@jukkaisokoski.fi <jukka@Jukkas-MacBook-Pro.local>
2021-01-01 20:27:10 +0200

    added login with google oauth, saving user data to DB, logout

--- routes/index.js
+++ routes/index.js
@@ -7,31 +7,31 @@ router.get('/', (req, res) => {
     res.render('login', {
         layout: 'login-layout'
     });
-})
+});
 
 // @desc    Dashboard
 // @route   GET / dashboard
 router.get('/dashboard', (req, res) => {
     res.render('dashboard');
-})
+});
 
 // @desc    Create new journal
 // @route   GET / create-journal
 router.get('/create-journal', (req, res) => {
     res.render('create-journal');
-})
+});
 
 // @desc    View journal
 // @route   GET / view-journal
 router.get('/view-journal', (req, res) => {
     res.render('view-journal');
-})
+});
 
 // @desc    Add new logs to journaö
 // @route   GET / add-log
 router.get('/add-log', (req, res) => {
     res.render('add-log');
-})
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 const monk = require('monk');
 const connectDB = require('./config/db');
 const exphbs = require('express-handlebars');
 const moment = require('moment');
 
 // Load config
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -24,12 +29,23 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main-layout', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5500;
 
