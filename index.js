bb009544b33b533bfb2725e133ae9dfcbc9c6958
mail@jukkaisokoski.fi <jukka@Jukkas-MacBook-Pro.local>
2021-01-01 13:34:25 +0200

    Added dotenv and cross-env dependencies, created config.env for storing env-variables

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 const monk = require('monk');
-var moment = require('moment');
+const moment = require('moment');
+
+// Load config
+dotenv.config({ path: './config/config.env' })
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 
@@ -14,7 +20,7 @@ const journals = db.get('journals');
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => console.log('Listening at port 5000'));
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 app.use(express.static('public'));
 
