ced186a917878a5efe60fe13bf44e4b5675df36e
aaxxiiss <mail@jukkaisokoski.fi>
2021-11-01 15:58:44 +0200

    removed prevent default from del-form

--- public/js/client.js
+++ public/js/client.js
@@ -1,20 +1,3 @@
-console.log('client.js loaded');
-
 function isPage(path) {
     return document.location.pathname === `/${path}`;
-}
-
-if (isPage('dashboard')) {
-    console.log('On Dashboard page');
-
-    const delForms = document.getElementsByClassName('delete-journal-form');
-    if (delForms) {
-        for (const form of delForms) {
-            form.addEventListener('submit', (event) => {
-                event.preventDefault();
-                console.log('Delete!')
-            });
-        }
-    }
-
 }
